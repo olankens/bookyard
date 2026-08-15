2dd65f869a4b8b43b9a3a51c51e9f5bd4d5df390
olankens <olankens@users.noreply.github.com>
2026-08-15 13:59:51 +0200

    Add in-memory authentication with password encoding

--- src/main/java/com/bookyard/config/SecurityConfig.java
+++ src/main/java/com/bookyard/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +25,23 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN").build();
+        var user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER").build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
